b3605e153a71382394fe60f5c44c13b129326a15
Jan <jan.madeyski@gmail.com>
2018-06-11 16:22:03 +0200

    Update select.vue

--- packages/select/src/select.vue
+++ packages/select/src/select.vue
@@ -200,7 +200,7 @@
           !this.multiple &&
           this.value !== undefined &&
           this.value !== '';
-        return criteria ? 'circle-close is-show-close' : (this.remote && this.filterable ? '' : 'arrow-up');
+        return criteria ? 'circle-close is-show-close' : 'arrow-up';
       },
 
       debounce() {
